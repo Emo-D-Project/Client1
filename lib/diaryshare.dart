c556f14e4057fe8bbe55137a7ccf31449c46d991
sudinggg <115580334+sudinggg@users.noreply.github.com>
2023-10-12 00:15:00 +0900

    [front] 일기 공유 틀

--- lib/diaryshare.dart
+++ lib/diaryshare.dart
@@ -5,6 +5,92 @@ class diaryshare extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding: EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin:  EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin:  EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin:  EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin:  EdgeInsets.all(8.0),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+                padding:  EdgeInsets.all(8.0),
+                margin:  EdgeInsets.all(8.0),
+              ),
+
+            ],
+          ),
+            color: Colors.grey,
+            width: 1000,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+
+        Container(
+          color: Colors.grey,
+          width: 1000,
+          height: 300,
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(8.0),
+        ),
+          Container(
+            color: Colors.grey,
+            width: 1000,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+        ],
+      )
+
+
+
+    );
   }
 }
