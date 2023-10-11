c38ce0e81162d116d0ef696a375a4a4c73549a03
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-10-11 19:11:36 +0900

    [front]권해진

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() async{
+
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key) ;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Text("화면"),
+    );
+  }
+}
