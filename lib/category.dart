8c5e34d6f09595058e470f9072d82a5d01e713a6
yewon <128056568+jeffyea320@users.noreply.github.com>
2023-10-11 23:45:39 +0900

    [front] 하단바 생성 & 페이지 이동 설정

---
+++ lib/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class category extends StatelessWidget {
+  const category({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
