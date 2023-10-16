7075308edae9cb015b2662262cc480c3b934a1d6
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-10-16 17:31:51 +0900

    [..]

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-class calendar extends StatelessWidget {
-  const calendar({super.key});
+class calendar extends StatefulWidget {
+  calendar({Key? key}) : super(key: key);
 
   @override
+  State<calendar> createState() => _calendarState();
+}
+
+class _calendarState extends State<calendar>{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF8F5EB),
-      body: Text("calendar"),
+      body: Container(
+        width: 380,
+        height: 600,
+        color: Color(0xFFB16409),
+      ),
     );
   }
 }
