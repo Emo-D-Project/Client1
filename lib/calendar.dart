4e77793cc3f22dea9060008c13e62d9ea5a11752
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-11-06 00:21:34 +0900

    [front] 디자인수정

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -18,163 +18,144 @@ class _calendarState extends State<calendar> {
     return Scaffold(
       backgroundColor: Color(0xFFF8F5EB),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                margin: EdgeInsets.fromLTRB(45, 0, 0, 0),
-                alignment: Alignment.centerLeft,
-                height: 30,
-                child: Text(
-                  '2023',
-                  style: TextStyle(
-                      color: Color(0xFF7D5A50),
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: 358,
-              height: 462,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Color(0xFFC5C4C4),
-                    spreadRadius: 2,
-                    blurRadius: 3,
-                    offset: Offset(4, 4),
-                  ),
-                ],
-                color: Color(0xFFD2C6BC),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: Container(
+          width: 358,
+          height: 530,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFFC5C4C4),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(4, 4),
               ),
-              child: TableCalendar(
-                //locale: 'ko_KR',
-                
-                rowHeight: 65,
-                firstDay: DateTime.utc(2021),
-                lastDay: DateTime.utc(2025),
-                focusedDay: DateTime.now(),
-                daysOfWeekHeight: 40,
-                weekendDays: [DateTime.sunday],
-                calendarBuilders: CalendarBuilders(
-                  dowBuilder: (context, day) {
-                    switch (day.weekday) {
-                      case 1:
-                        return Center(
-                          child: Text(
-                            'M',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 2:
-                        return Center(
-                          child: Text(
-                            'T',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 3:
-                        return Center(
-                          child: Text(
-                            'W',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 4:
-                        return Center(
-                          child: Text(
-                            'T',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 5:
-                        return Center(
-                          child: Text(
-                            'F',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 6:
-                        return Center(
-                          child: Text(
-                            'S',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      case 7:
-                        return Center(
-                          child: Text(
-                            'S',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.red),
-                          ),
-                        );
-                    }
-                  },
-                ),
-                headerStyle: HeaderStyle(
-                  formatButtonVisible: false,
-                  titleCentered: true,
-                  rightChevronIcon: Icon(Icons.arrow_forward_ios_outlined,
-                      color: Color(0xFF7D5A50)),
-                  leftChevronIcon: Icon(Icons.arrow_back_ios_new_outlined,
-                      color: Color(0xFF7D5A50)),
-                  titleTextFormatter: (date, locale) =>
-                      DateFormat.MMMM(locale).format(date).toUpperCase(),
-                  headerMargin: EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  titleTextStyle: TextStyle(
-                      color: Color(0xFF7C5A50),
-                      fontWeight: FontWeight.w900,
-                      fontSize: 25),
-                ),
-                calendarStyle: CalendarStyle(
-                  cellAlignment: Alignment.center,
-                  isTodayHighlighted: true,
-                  outsideDaysVisible: false,
-                  weekendTextStyle: const TextStyle(
-                    color: Colors.red,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  tableBorder: const TableBorder(
-                    horizontalInside: BorderSide(color: Colors.white),
-                  ),
-                  defaultTextStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                  todayTextStyle: TextStyle(color: Colors.transparent),
-                  todayDecoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('images/emotion/1.gif'))
-                  ),
+            ],
+            color: Color(0xFFD2C6BC),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: TableCalendar(
+            //locale: 'ko_KR',
+
+            rowHeight: 65,
+            firstDay: DateTime.utc(2021),
+            lastDay: DateTime.utc(2025),
+            focusedDay: DateTime.now(),
+            daysOfWeekHeight: 40,
+            weekendDays: [DateTime.sunday],
+            calendarBuilders: CalendarBuilders(
+              dowBuilder: (context, day) {
+                switch (day.weekday) {
+                  case 1:
+                    return Center(
+                      child: Text(
+                        'M',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 2:
+                    return Center(
+                      child: Text(
+                        'T',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 3:
+                    return Center(
+                      child: Text(
+                        'W',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 4:
+                    return Center(
+                      child: Text(
+                        'T',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 5:
+                    return Center(
+                      child: Text(
+                        'F',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 6:
+                    return Center(
+                      child: Text(
+                        'S',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  case 7:
+                    return Center(
+                      child: Text(
+                        'S',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.red),
+                      ),
+                    );
+                }
+              },
+            ),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              rightChevronIcon: Icon(Icons.arrow_forward_ios_outlined,
+                  color: Color(0xFF7D5A50)),
+              leftChevronIcon: Icon(Icons.arrow_back_ios_new_outlined,
+                  color: Color(0xFF7D5A50)),
+              titleTextFormatter: (date, locale) =>
+                  DateFormat.yMMM(locale).format(date).toUpperCase(),
+              headerMargin: EdgeInsets.fromLTRB(50, 15, 50, 15),
+              titleTextStyle: TextStyle(
+                  color: Color(0xFF7C5A50),
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25),
+            ),
+            calendarStyle: CalendarStyle(
+              cellAlignment: Alignment.center,
+              isTodayHighlighted: true,
+              outsideDaysVisible: false,
+              weekendTextStyle: const TextStyle(
+                color: Colors.red,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              tableBorder: const TableBorder(
+                horizontalInside: BorderSide(color: Colors.white),
+              ),
+              defaultTextStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+              todayTextStyle: TextStyle(color: Colors.transparent),
+              todayDecoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/emotion/1.gif'))
+              ),
 
 
-                ),
-              ),
             ),
-          ],
+          ),
         ),
       ),
     );
