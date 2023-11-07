f7446d1991d1a94f9e0c56286f906ab1caaaf1e6
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-11-07 21:00:21 +0900

    ..

--- lib/tokenManager.dart
+++ lib/tokenManager.dart
@@ -1,29 +1,16 @@
 import 'network/api_manager.dart';
 
 class TokenManager {
-  static TokenManager? _instance;
+  static TokenManager tokenManager = new TokenManager();
   late String accessToken;
   late String refreshToken;
 
-  ApiManager apiManager = ApiManager().getApiManager();
-
-  TokenManager._(); // private constructor
-
-  factory TokenManager() {
-    if (_instance == null) {
-      _instance = TokenManager._();
-    }
-    return _instance!;
+  TokenManager getTokenManager(){
+    
   }
 
-  Future<void> getServerToken(String kakaoToken) async {
-    try {
-      final response = await apiManager.getServerToken(kakaoToken);
 
-      accessToken = response["access_token"];
-      refreshToken = response["refresh_token"];
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
+
+
+
 }
