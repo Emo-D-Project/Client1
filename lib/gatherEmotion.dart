72495c5a74b27df0467bc66211f80ca0e8b4dd03
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-11-28 17:52:17 +0900

    완

--- lib/gatherEmotion.dart
+++ lib/gatherEmotion.dart
@@ -1,16 +1,34 @@
 import 'package:capston1/diaryReplay.dart';
 import 'package:flutter/material.dart';
+import 'network/api_manager.dart';
 import 'statistics.dart';
 import 'package:intl/intl.dart';
 
 List<DateTime> title = [DateTime(2023, 10)];
 
-List<String> content = ["1011","1025","1031","1025","1025","1025","1025","1025","1025"];
+List<String> content = [
+  "1011",
+  "1025",
+  "1031",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+  "1025",
+
+];
 
 List<String> emo = [
   'images/emotion/1.gif',
   'images/emotion/2.gif',
   'images/emotion/1.gif',
+  'images/emotion/2.gif',
+  'images/emotion/2.gif',
+  'images/emotion/2.gif',
   'images/emotion/2.gif',
   'images/emotion/2.gif',
   'images/emotion/2.gif',
@@ -26,16 +44,60 @@ class gatherEmotion extends StatefulWidget {
   State<gatherEmotion> createState() => _gatherEmotionState();
 }
 
-final LayerLink _layerLink = LayerLink();
-
 class _gatherEmotionState extends State<gatherEmotion> {
-  List<String> items = ['기본', '기쁨', '슬픔', '화남', '짜증', '피곤', '설렘'];
-  String? selectedItem = '기본';
+  ApiManager apiManager = ApiManager().getApiManager();
+
+  Map<DateTime, String> _events = {
+  };// map.getkey를 해서 10월에 맞는 날짜를 추출해서 리스트로 만들고 쓰기
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromServer();
+  }
+
+  Future<void> fetchDataFromServer() async {
+    try {
+      final data = await apiManager.getCalendarData();
+
+      setState(() {
+        _events = data!;
+      });
+    } catch (error) {
+      // Handle error
+      //print('Error fetching data: $error');
+      print('Error fetching data: ${error.toString()}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final SizeX = MediaQuery.of(context).size.width;
     final SizeY = MediaQuery.of(context).size.height;
+
+
+    Map<DateTime, String> sortedMap = Map.fromEntries(
+        _events.entries.toList()..sort((e1, e2) => e1.key.compareTo(e2.key)));
+
+    List<DateTime> dateList = sortedMap.keys.toList();
+    List<String> valueList = sortedMap.values.toList();
+
+    List<DateTime> yearMonthList = [];
+    _events.forEach((key, value) {
+      bool isCheck = false;
+
+      yearMonthList.forEach((element) {
+          if(element.year == key.year && element.month == key.month ){
+            isCheck = true;
+          }
+        });
+      // 겹치는 값이 없으면
+      if(isCheck == false){
+        yearMonthList.add(key);
+      }
+    });
+
+
 
     return Scaffold(
       backgroundColor: Color(0xFFF8F5EB),
@@ -55,85 +117,96 @@ class _gatherEmotionState extends State<gatherEmotion> {
           icon: Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: SizedBox(
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-              //드롭박스
-              color: Colors.blueGrey,
-            ),
-            Text("2023.10"),
-            Container(
-              margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-              child: Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  //padding: const EdgeInsets.all(10),
-                  itemCount: title.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return MyCustomContent(gtitle: title[index],gcontent: content, gemo: emo);
-                  },
-                ),
+      body: ListView.builder(
+          itemCount: yearMonthList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    DateFormat('yyyy.MM').format(yearMonthList[index]),
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'soojin',
+                      color: Colors.brown,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 60,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: dateList.length,
+                      itemBuilder: (BuildContext context, int subIndex) {
+                        return Container(
+                          margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                          child: Column(
+                            children: [
+                              Container(
+                                child: (() {
+                                  switch (valueList[subIndex]){
+                                    case "smile":
+                                      return Image.asset(
+                                        'images/emotion/1.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "flutter":
+                                      return Image.asset(
+                                        'images/emotion/2.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "angry":
+                                      return Image.asset(
+                                        'images/emotion/angry.png',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "annoying":
+                                      return Image.asset(
+                                        'images/emotion/4.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "tired":
+                                      return Image.asset(
+                                        'images/emotion/5.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "sad":
+                                      return Image.asset(
+                                        'images/emotion/6.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                    case "calmness":
+                                      return Image.asset(
+                                        'images/emotion/7.gif',
+                                        width: 40,
+                                        height: 40,
+                                      );
+                                  }
+                                })(),
+                              ),
+                              Text('${dateList[subIndex].month}${dateList[subIndex].day}',
+                                style: TextStyle(fontFamily: 'soojin', fontSize: 15, color: Colors.brown),)
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MonthlyData {
-  final DateTime title;
-  final List<String> content;
-  final List<String> emo;
-
-  MonthlyData({
-    required this.title,
-    required this.content,
-    required this.emo,
-  });
-}
-
-//커스텀 컨테이너
-class MyCustomContent extends StatelessWidget {
-  final DateTime gtitle;
-  final List<String> gcontent;
-  final List<String> gemo;
-
-  MyCustomContent({
-    super.key,
-    required this.gtitle,
-    required this.gcontent,
-    required this.gemo,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 500,
-      height: 100,
-      child: Row(
-        children: [
-          Expanded(
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  //shrinkWrap: true,
-                  itemCount: gemo.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      children: [
-                        IconButton(
-                            iconSize: 40, onPressed: () {}, icon: Image.asset("${gemo[index]}")),
-                        Text(gcontent[index])
-                      ],
-                    );
-                  },
-              )
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
