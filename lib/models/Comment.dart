c45cdc945226b4fec446b324e94b2dc6a88f0c2d
sudinggg <115580334+sudinggg@users.noreply.github.com>
2023-11-30 18:37:08 +0900

    일기공유 언제 끝나지

--- lib/models/Comment.dart
+++ lib/models/Comment.dart
@@ -5,7 +5,7 @@ class Comment {
   int user_id;
 
   Comment({
-    this.post_id =0,   //다이어리 아이디
+    this.post_id = 0,   //다이어리 아이디
     required this.content,  //댓글 내용
     this.id = 0,
     this.user_id=0, //댓글 쓴 사람
