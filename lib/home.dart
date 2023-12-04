e1a529d453c34978369913efb3873a8360c49bdb
yewon <128056568+jeffyea320@users.noreply.github.com>
2023-12-04 12:17:10 +0900

    [front] 홈화면 적용중

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,118 @@
 import 'package:capston1/writediary.dart';
 import 'package:flutter/material.dart';
+import 'models/Diary.dart';
+import 'network/api_manager.dart';
 
-class home extends StatelessWidget {
+class home extends StatefulWidget {
   const home({super.key});
 
   @override
+  State<home> createState() => _homeState();
+}
+
+DateTime now = DateTime.now();
+
+class _homeState extends State<home> {
+  ApiManager apiManager = ApiManager().getApiManager();
+  List<Diary> diaries = [];
+  String emotionToday = ''; // late 키워드를 사용해 초기화를 뒤로 미룸
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromServer();
+  }
+
+
+  Future<void> fetchDataFromServer() async {
+    try {
+      final data = await apiManager.getDiaryShareData();
+
+      setState(() {
+        diaries = data; // fetchDataFromServer()가 완료된 후에 감정을 설정함
+        emotionToday = diaries
+            .firstWhere(
+              (diary) =>
+          diary.date.year == now.year &&
+              diary.date.month == now.month &&
+              diary.date.day == now.day,
+          orElse: () => Diary(
+            date: DateTime.now(),
+            content: '',
+            emotion: 'calmness', // 기본 감정 설정
+          ),
+        ).emotion;
+
+      });
+    } catch (error) {
+      // 에러 제어하는 부분
+      print('Error getting share diaries list: $error');
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    print("emotionToday: $emotionToday");
+
+    if (emotionToday == null) {
+      return CircularProgressIndicator(); // 데이터를 기다리는 동안 로딩 표시
+    }
+
     return Scaffold(
       backgroundColor: Color(0xFFF8F5EB),
       body: Center(
-          child: SizedBox(
-        child: Image.asset(
-          'images/main/catmovereal.gif',
-          width: 1200,
-          height: 1000,
-          fit: BoxFit.contain,
-        ),
-      )),
+        child: SizedBox(
+            child: (() {
+          switch (emotionToday) {
+            case 'flutter':
+              return Image.asset(
+                'images/main/flutter.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'angry':
+              return Image.asset(
+                'images/main/angry.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'annoying':
+              return Image.asset(
+                'images/main/anoying.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'smile':
+              return Image.asset(
+                'images/main/smile.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'tired':
+              return Image.asset(
+                'images/main/tired.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'calmness':
+              return Image.asset(
+                'images/main/catmovereal.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );case 'sad':
+              return Image.asset(
+                'images/main/catmovereal.gif',
+                width: 1200,
+                height: 1000,
+                fit: BoxFit.contain,
+              );
+            default:
+              return Container();
+          }
+        }())),
+      ),
       floatingActionButton: Builder(builder: (context) {
         return FloatingActionButton(
           backgroundColor: Color(0xFFD2C6BC),
