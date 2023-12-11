f1d15a1f6d9cb9c4aafba74af29b9c867a336786
sudinggg <115580334+sudinggg@users.noreply.github.com>
2023-12-11 19:14:49 +0900

    6

--- lib/test2/style.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-var theme = ThemeData(
-  scaffoldBackgroundColor: Color.fromRGBO(248, 245, 235, 100),
-  appBarTheme: AppBarTheme(
-      iconTheme: IconThemeData(
-    color: Color(0xFF57642B),
-  )),
-);
